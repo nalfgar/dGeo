495ab8619c44fbb4a4de7735d2ba66cf0b726f90
Darek Strojecki <dstroj@wp.pl>
2018-03-05 16:08:23 +0100

    Work witch HForm class. Change form to multidemensional. Not finish yet.

--- src/main/java/HForm.java
+++ src/main/java/HForm.java
@@ -1,96 +1,100 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HForm {
     private static final double ZERO = 0.0;
-    private double a;
-    private double b;
-    private double c;
-    private double d;
-
+    private List<List<Double>> data = new ArrayList<List<Double>>();
 
     //    Primal forms
     public double firstForm() {
-        return (a * d) - (b * c);
+        double sum = 0.0;
+        for (List<Double> form : data) {
+            sum += (form.get(0) * form.get(3)) - (form.get(1) * form.get(2));
+        }
+        return sum;
     }
 
     public double secondForm() {
-        return (a * c) + (b * d);
+        double sum = 0.0;
+        for (List<Double> form : data) {
+            sum += (form.get(0) * form.get(2)) + (form.get(1) * form.get(3));
+        }
+
+        return sum;
     }
 
     //    Complementary forms
     public double zeroForm() {
         if (secondForm() == ZERO) return ZERO;
+
         return firstForm() / secondForm();
     }
 
-    public double firstFormDownRound() {
-        double denominator = c + d;
+    public double firstFormBottomRound() {
+        double denominator = 0.0;
+        for (List<Double> form : data) {
+            denominator += (form.get(2) +form.get(3));
+        }
         if (denominator == ZERO) return ZERO;
         return firstForm() / denominator;
     }
-
-    public double secondFormDownRound() {
-        double denominator = c + d;
-        if (denominator == ZERO) return ZERO;
-        return secondForm() / denominator;
-    }
-
-    public double firstFormTopRound() {
-        double denominator = a + b;
-        if (denominator == ZERO) return ZERO;
-        return firstForm() / denominator;
-    }
-
-    public double secondFormTopRound() {
-        double denominator = a + b;
-        if (denominator == ZERO) return ZERO;
-        return secondForm() / denominator;
-    }
-
-    public double firstFormDownSquare() {
-        double denominator = (c * c) + (d * d);
-        if (denominator == ZERO) return ZERO;
-        return firstForm() / denominator;
-    }
-
-    public double secondFormDownSquare() {
-        double denominator = (c * c) + (d * d);
-        if (denominator == ZERO) return ZERO;
-        return secondForm() / denominator;
-    }
-
-    public double firstFormTopSquare() {
-        double denominator = (a * a) + (b * b);
-        if (denominator == ZERO) return ZERO;
-        return firstForm() / denominator;
-    }
-
-    public double secondFormTopSquare() {
-        double denominator = (a * a) + (b * b);
-        if (denominator == ZERO) return ZERO;
-        return secondForm() / denominator;
-    }
+//
+//    public double secondFormBottomRound() {
+//        double denominator = c + d;
+//        if (denominator == ZERO) return ZERO;
+//        return secondForm() / denominator;
+//    }
+//
+//    public double firstFormTopRound() {
+//        double denominator = a + b;
+//        if (denominator == ZERO) return ZERO;
+//        return firstForm() / denominator;
+//    }
+//
+//    public double secondFormTopRound() {
+//        double denominator = a + b;
+//        if (denominator == ZERO) return ZERO;
+//        return secondForm() / denominator;
+//    }
+//
+//    public double firstFormBottomSquare() {
+//        double denominator = (c * c) + (d * d);
+//        if (denominator == ZERO) return ZERO;
+//        return firstForm() / denominator;
+//    }
+//
+//    public double secondFormBottomSquare() {
+//        double denominator = (c * c) + (d * d);
+//        if (denominator == ZERO) return ZERO;
+//        return secondForm() / denominator;
+//    }
+//
+//    public double firstFormTopSquare() {
+//        double denominator = (a * a) + (b * b);
+//        if (denominator == ZERO) return ZERO;
+//        return firstForm() / denominator;
+//    }
+//
+//    public double secondFormTopSquare() {
+//        double denominator = (a * a) + (b * b);
+//        if (denominator == ZERO) return ZERO;
+//        return secondForm() / denominator;
+//    }
 
     public HForm(double a, double b, double c, double d) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
+        List<Double> form = new ArrayList<Double>();
+        form.add(a);
+        form.add(b);
+        form.add(c);
+        form.add(d);
+        data.add(form);
+        }
+
+    public HForm(List<Double> form) {
+        data.add(form);
     }
 
-    public double getA() {
-        return a;
+    public void add(List<Double> form) {
+        data.add(form);
     }
-
-    public double getB() {
-        return b;
-    }
-
-    public double getC() {
-        return c;
-    }
-
-    public double getD() {
-        return d;
-    }
-
-
 }
--- src/test/java/HFormTest.java
+++ src/test/java/HFormTest.java
@@ -1,26 +1,49 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class HFormTest {
     private HForm hForm0;
     private HForm hForm1;
+    private HForm hForm2;
+    private HForm hForm3;
+    private HForm hForm4;
+    private HForm hForm5;
 
     @Before
     public void setup() {
-        hForm0 = new HForm(0.0, 0.0, 0.0, 0.0);
+        hForm0 = new HForm( 0.0, 0.0, 0.0, 0.0);
         hForm1 = new HForm(10.0, 20.0, 30.0, 40.0);
+
+        List<Double> form0 = new ArrayList<Double>();
+        form0.add(0.0);
+        form0.add(0.0);
+        form0.add(0.0);
+        form0.add(0.0);
+        List<Double> form1 = new ArrayList<Double>();
+        form1.add(10.0);
+        form1.add(20.0);
+        form1.add(30.0);
+        form1.add(40.0);
+        hForm2 = new HForm(form0);
+        hForm3 = new HForm(form1);
+        hForm4 = new HForm(form0);
+        hForm4.add(form0);
+        hForm5 = new HForm(form1);
+        hForm5.add(form1);
     }
 
     @Test
-    public void testConstructor() {
+    public void testFirstFormZero() {
 
-        assertThat(hForm0.getA()).isEqualTo(0.0);
-        assertThat(hForm0.getB()).isEqualTo(0.0);
-        assertThat(hForm0.getC()).isEqualTo(0.0);
-        assertThat(hForm0.getD()).isEqualTo(0.0);
+        assertThat(hForm0.firstForm()).isEqualTo(0.0);
+        assertThat(hForm2.firstForm()).isEqualTo(0.0);
+        assertThat(hForm4.firstForm()).isEqualTo(0.0);
     }
 
 
@@ -28,108 +51,114 @@ public class HFormTest {
     public void testFirstForm() {
 
         assertThat(hForm1.firstForm()).isEqualTo(-200.0);
+        assertThat(hForm3.firstForm()).isEqualTo(-200.0);
+        assertThat(hForm5.firstForm()).isEqualTo(-400.0);
     }
 
     @Test
     public void testSecondForm() {
 
         assertThat(hForm1.secondForm()).isEqualTo(1100.0);
+        assertThat(hForm5.secondForm()).isEqualTo(2200.0);
     }
 
     @Test
     public void testZeroFormZero() {
 
         assertThat(hForm0.zeroForm()).isEqualTo(0.0);
+        assertThat(hForm2.zeroForm()).isEqualTo(0.0);
     }
 
     @Test
     public void testZeroFormNotZero() {
 
         assertThat(hForm1.zeroForm()).isEqualTo(-200.0 / 1100.0);
+        assertThat(hForm5.zeroForm()).isEqualTo(-400.0 / 2200.0);
     }
 
     @Test
-    public void testFirstFormDownRoundZero() {
+    public void testFirstFormBottomRoundZero() {
 
-        assertThat(hForm0.firstFormDownRound()).isEqualTo(0.0);
+        assertThat(hForm0.firstFormBottomRound()).isEqualTo(0.0);
     }
 
     @Test
-    public void testFirstFormDownRoundNotZero() {
+    public void testFirstFormBottomRoundNotZero() {
 
-        assertThat(hForm1.firstFormDownRound()).isEqualTo(-200.0 / 70.0);
+        assertThat(hForm1.firstFormBottomRound()).isEqualTo(-200.0 / 70.0);
+        assertThat(hForm5.firstFormBottomRound()).isEqualTo(-200.0 / 70.0);
     }
 
-    @Test
-    public void testSecondFormDownRoundZero() {
-
-        assertThat(hForm0.secondFormDownRound()).isEqualTo(0.0);
-    }
-
-    @Test
-    public void testSecondFormDownRoundNotZero() {
-
-        assertThat(hForm1.secondFormDownRound()).isEqualTo(1100.0 / 70.0);
-    }
-
-    @Test
-    public void testFirstFormTopRoundZero() {
-        assertThat(hForm0.firstFormTopRound()).isEqualTo(0.0);
-    }
-
-    @Test
-    public void testFirstFormTopRoundNotZero() {
-        assertThat(hForm1.firstFormTopRound()).isEqualTo(-200.0 / 30.0);
-    }
-
-    @Test
-    public void testSecondFormTopRoundZero() {
-        assertThat(hForm0.secondFormTopRound()).isEqualTo(0.0);
-    }
-
-    @Test
-    public void testSecondFormTopRoundNotZero() {
-        assertThat(hForm1.secondFormTopRound()).isEqualTo(1100.0 / 30.0);
-    }
-
-    @Test
-    public void testFirstFormDownSquareZero() {
-        assertThat(hForm0.firstFormDownSquare()).isEqualTo(0.0);
-    }
-
-    @Test
-    public void testFirstFormDownSquareNotZero() {
-        assertThat(hForm1.firstFormDownSquare()).isEqualTo(-200.0 / (900.0 + 1600.0));
-    }
-
-    @Test
-    public void testSeconfFormDownSquareZero() {
-        assertThat(hForm0.secondFormDownSquare()).isEqualTo(0.0);
-    }
-
-    @Test
-    public void testSeconfFormDownSquareNotZero() {
-        assertThat(hForm1.secondFormDownSquare()).isEqualTo(1100.0 / (900.0 + 1600.0));
-    }
-
-    @Test
-    public void testFirstFormTopSquareZero() {
-        assertThat(hForm0.firstFormTopSquare()).isEqualTo(0.0);
-    }
-
-    @Test
-    public void testFirstFormTopSquareNotZero() {
-        assertThat(hForm1.firstFormTopSquare()).isEqualTo(-200.0 / (100 + 400));
-    }
-
-    @Test
-    public void testSecondFormTopSquareZero() {
-        assertThat(hForm0.secondFormTopSquare()).isEqualTo(0.0);
-    }
-
-    @Test
-    public void testSecondFormTopSquareNotZero() {
-        assertThat(hForm1.secondFormTopSquare()).isEqualTo(1100.0/(100+400));
-    }
+//    @Test
+//    public void testSecondFormBottomRoundZero() {
+//
+//        assertThat(hForm0.secondFormBottomRound()).isEqualTo(0.0);
+//    }
+//
+//    @Test
+//    public void testSecondFormBottomRoundNotZero() {
+//
+//        assertThat(hForm1.secondFormBottomRound()).isEqualTo(1100.0 / 70.0);
+//    }
+//
+//    @Test
+//    public void testFirstFormTopRoundZero() {
+//        assertThat(hForm0.firstFormTopRound()).isEqualTo(0.0);
+//    }
+//
+//    @Test
+//    public void testFirstFormTopRoundNotZero() {
+//        assertThat(hForm1.firstFormTopRound()).isEqualTo(-200.0 / 30.0);
+//    }
+//
+//    @Test
+//    public void testSecondFormTopRoundZero() {
+//        assertThat(hForm0.secondFormTopRound()).isEqualTo(0.0);
+//    }
+//
+//    @Test
+//    public void testSecondFormTopRoundNotZero() {
+//        assertThat(hForm1.secondFormTopRound()).isEqualTo(1100.0 / 30.0);
+//    }
+//
+//    @Test
+//    public void testFirstFormBottomSquareZero() {
+//        assertThat(hForm0.firstFormBottomSquare()).isEqualTo(0.0);
+//    }
+//
+//    @Test
+//    public void testFirstFormBottomSquareNotZero() {
+//        assertThat(hForm1.firstFormBottomSquare()).isEqualTo(-200.0 / (900.0 + 1600.0));
+//    }
+//
+//    @Test
+//    public void testSeconfFormBottomSquareZero() {
+//        assertThat(hForm0.secondFormBottomSquare()).isEqualTo(0.0);
+//    }
+//
+//    @Test
+//    public void testSeconfFormBottomSquareNotZero() {
+//        assertThat(hForm1.secondFormBottomSquare()).isEqualTo(1100.0 / (900.0 + 1600.0));
+//    }
+//
+//    @Test
+//    public void testFirstFormTopSquareZero() {
+//        assertThat(hForm0.firstFormTopSquare()).isEqualTo(0.0);
+//    }
+//
+//    @Test
+//    public void testFirstFormTopSquareNotZero() {
+//        assertThat(hForm1.firstFormTopSquare()).isEqualTo(-200.0 / (100 + 400));
+//    }
+//
+//    @Test
+//    public void testSecondFormTopSquareZero() {
+//        assertThat(hForm0.secondFormTopSquare()).isEqualTo(0.0);
+//    }
+//
+//    @Test
+//    public void testSecondFormTopSquareNotZero() {
+//        assertThat(hForm1.secondFormTopSquare()).isEqualTo(1100.0/(100+400));
+//    }
 
 }
